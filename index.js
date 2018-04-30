6c1ae556b788371a9dc94abc42d845ec13ce6389
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-30 21:56:54 +0000

    Done.

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ function breakOut(array, changeValue, stopValue) {
     }
     array[i] = changeValue
   }
+  return array
 }
 
 function keepGoing(array, changeValue, skipValue) {
-
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] != skipValue) {
+      array[i] = changeValue
+    }
+  }
+  return array
 }
 
 function findBy(array, findFn) {
-
+  for (var i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+  return null
 }
